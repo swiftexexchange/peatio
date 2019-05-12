2154c12fe6a9d841da6ddd510a7fa10ca1d14ead
joenilan <joenilan@gmail.com>
2019-05-12 17:50:25 -0400

    update

---
+++ app/services/blockchain_service/smrtc.rb
@@ -0,0 +1,9 @@
+# encoding: UTF-8
+# frozen_string_literal: true
+
+module BlockchainService
+  class Smrtc < Bitcoin
+
+  end
+end
+
---
+++ lib/peatio/blockchain_client/veles.rb
@@ -0,0 +1,8 @@
+# encoding: UTF-8
+# frozen_string_literal: true
+
+module BlockchainClient
+  class Veles < Bitcoin
+
+  end
+end
