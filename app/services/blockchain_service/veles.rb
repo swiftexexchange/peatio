2c334645627e1fec25254305f8922cd3901bb0ab
joenilan <joenilan@gmail.com>
2019-05-12 17:46:25 -0400

    custom code

---
+++ app/services/blockchain_service/veles.rb
@@ -0,0 +1,9 @@
+# encoding: UTF-8
+# frozen_string_literal: true
+
+module BlockchainService
+  class Veles < Bitcoin
+
+  end
+end
+
---
+++ app/services/wallet_service/mymnd.rb
@@ -0,0 +1,8 @@
+# encoding: UTF-8
+# frozen_string_literal: true
+
+module WalletService
+  class Mymnd < Bitcoind
+
+  end
+end
