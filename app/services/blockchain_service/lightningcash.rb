42bce637b21a135650795f0426835fbaba63c64a
Joe Nilan <joenilan@gmail.com>
2019-06-27 13:49:20 -0400

    Update lightningcash.rb

--- app/services/blockchain_service/lightningcash.rb
+++ app/services/blockchain_service/lightningcash.rb
@@ -2,7 +2,7 @@
 # frozen_string_literal: true
 
 module BlockchainService
-  class LightningCash < Base
+  class Lightningcash < Base
     # Rough number of blocks per hour for Bitcoin is 6.
     def process_blockchain(blocks_limit: 6, force: false)
       latest_block = client.latest_block_number
